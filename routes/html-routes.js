57741a70e1da93311324b9e49a09932258b4b3b4
gitrenn <rjingmembers@gmail.com>
2017-12-11 14:14:39 -0800

    created folders files and dependencies

---
+++ routes/html-routes.js
@@ -0,0 +1,15 @@
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+
+  // index route loads view.html
+ 
+  // create other routers to different pages in the following 
+
+};
